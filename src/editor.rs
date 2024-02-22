68c0dd7e28d2d7ad6141dcd5bd9d70ab4d3dc4c6
Krish Mittal <krishmittal212005@gmail.com>
2024-02-22 09:11:00 +0530

    clear screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
@@ -11,16 +11,32 @@ impl Editor {
     pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
         loop {
-            if let Err(error) = self.process_keypress() {
+            if let Err(error) = self.refresh_screen() {
                 die(error);
             }
             if self.should_quit {
                 break;
             }
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
         }
     }
     pub fn default() -> Self {
         Self { should_quit: false }
+    }
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        //print!("\x1b[2j"); escape sequence -> \x1b is the escape character or 27 in decimal. j
+        // -> to clear the screen 2-> argument that says clear the entire screen.
+        // https://vt100.net/docs/vt100-ug/chapter3.html
+        // termion allows to not write the escape sequence
+
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1)); // goto using escape
+                                                                          // sequence H command -> (row no, col no) at which to position the cursor, 1-based
+        if self.should_quit {
+            println!("Goodbye.\r");
+        }
+        io::stdout().flush()
     }
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         let pressed_key = read_key()?; // ? says if there is an error return it if not then
@@ -40,5 +56,6 @@ fn read_key() -> Result<Key, std::io::Error> {
     }
 }
 fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{}", e);
 }
