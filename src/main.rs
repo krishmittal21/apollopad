997d6693d13cadc6a22fa9803ea6ff8399759382
Krish Mittal <krishmittal212005@gmail.com>
2024-02-22 08:17:20 +0530

    refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-fn to_die(e: std::io::Error) {
-    panic!("{}", e);
-}
+use editor::Editor;
+mod editor;
+
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => to_die(err),
-        }
-    }
+    let editor = Editor {};
+    editor.run();
 }
